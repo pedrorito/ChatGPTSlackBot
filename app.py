afc72565e36d7c8fa1a1d8001bf6360422e19d32
Gonzalo Sanchez <gonchi.sanchez@gmail.com>
2023-03-17 15:11:08 -0300

    Make it less verbose

--- app.py
+++ app.py
@@ -7,26 +7,23 @@ from revChatGPT.V3 import Chatbot
 from slack_bolt import App
 
 ChatGPTConfig = {
-        "api_key": os.getenv("OPENAI_API_KEY"),
-    }
+    "api_key": os.getenv("OPENAI_API_KEY"),
+}
+
 if os.getenv("OPENAI_ENGINE"):
     ChatGPTConfig["engine"] = os.getenv("OPENAI_ENGINE")
 
 app = App()
 chatbot = Chatbot(**ChatGPTConfig)
 
-# Listen for an event from the Events API
+
 @app.event("app_mention")
 def event_test(event, say):
     prompt = re.sub('(?:\s)<@[^, ]*|(?:^)<@[^, ]*', '', event['text'])
     try:
         response = chatbot.ask(prompt)
         user = event['user']
-        user = f"<@{user}> you asked:"
-        asked = ['>',prompt]
-        asked = "".join(asked)
-        send = [user,asked,response]
-        send = "\n".join(send)
+        send = f"@{user}: {response}"
     except Exception as e:
         print(e)
         send = "We're experiencing exceptionally high demand. Please, try again."
@@ -37,9 +34,11 @@ def event_test(event, say):
     # Use the `app.event` method to send a reply to the message thread
     say(send, thread_ts=original_message_ts)
 
+
 def chatgpt_refresh():
     while True:
         time.sleep(60)
+
 
 if __name__ == "__main__":
     thread = Thread(target=chatgpt_refresh)
